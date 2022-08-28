8f6eaeeac588addbc78feed48a50cdb45b23847b
ashifsadiq <ashifsadiq@gmail.com>
2022-08-28 18:59:42 +0530

    start

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { inherits } = require('util')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(5000)
+git init
+git add.
+git commit - m "fkjbf"
+git remote add origin git @github.com: ashifsadiq / 5000.git
+git branch - M main
+git push - u origin main
